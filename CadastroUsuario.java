f0c03853559bc6363e9d5dacdb4f41631c51947e
luanagreys7 <luanagreys7@gmail.com>
2025-08-04 10:02:56 -0300

    .

---
+++ CadastroUsuario.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import javax.swing.*;
+
+// Ccadastro de usuário
+public class CadastroUsuario {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Cadastro de Usuário");
+        
+    }
+}
